a3605cd6d874c19f3ce168786ff175157ac203d4
Sarthak dhoke <dksarthak101@gmail.com>
2025-05-01 22:52:35 +0530

    started useEffect hool

--- react-practicing-pro/src/components/useEffectHook/UseEffecthook.jsx
+++ react-practicing-pro/src/components/useEffectHook/UseEffecthook.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const UseEffecthook = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>UseEffecthook</h1>
+    </>
+  );
 };
 
 export default UseEffecthook;
