ed39f5dc9d4a7d91259aa554ba6d2b8e6d4aeb04
Sarthak dhoke <dksarthak101@gmail.com>
2025-05-16 22:45:49 +0530

    done use hook

--- react-practicing-pro/src/components/ContextAPI/ContextHook.jsx
+++ react-practicing-pro/src/components/ContextAPI/ContextHook.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+// import React, { useContext } from "react";
+// import { BioContext, useBioContext } from "react";
+import { BioContext, use } from "react";
 
 const ContextHook = () => {
+  // in case of context API
+  // const { myName, age } = useContext(BioContext);
+
+  // in case of custom hook
+  // const { myName, age } = useBioContext();
+
+  // in case of use hook
+  // const { myName, age } = use(BioContext);
+
+  // in case of use hook with condition
+  const newHook = true;
+  let myName, age;
+  if (newHook) {
+    ({ myName, age } = use(BioContext));
+  }
+
   return (
     <>
-      <h1>hellloooo</h1>
+      <h1>
+        hellloooo I'm {myName} and {age} old
+      </h1>
     </>
   );
 };
--- react-practicing-pro/src/components/ContextAPI/Index.jsx
+++ react-practicing-pro/src/components/ContextAPI/Index.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+// import { createContext, useContext } from "react";
+import { createContext, use } from "react";
 
 // step -1
 export const BioContext = createContext();
@@ -6,8 +7,25 @@ export const BioContext = createContext();
 // step -2
 export const BioProvider = ({ children }) => {
   const myName = "Rahul";
+  const age = 20;
 
   console.log(children);
 
-  return <BioContext.Provider value={myName}>{children}</BioContext.Provider>;
+  return (
+    <BioContext.Provider value={{ myName: myName, age: age }}>
+      {children}
+    </BioContext.Provider>
+  );
+};
+
+// Custom Hook
+export const useBioContext = () => {
+  // const context = useContext(BioContext);
+
+  // in case of use hook
+  const context = use(BioContext);
+  if (context === undefined) {
+    throw new Error("component must be used within a BioProvider");
+  }
+  return context;
 };
